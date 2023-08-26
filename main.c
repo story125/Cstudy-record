620f95742eb44ab3ff3f99a246e27a0274b83aeb
Koduhwan <rhenghks5167@naver.com>
2023-08-26 16:30:51 +0900

    day_3

--- main.c
+++ main.c
@@ -2,13 +2,11 @@
 
  void main() {
 
-      int data1, data2;
-      data1 = 5 , data2 = 2 ;
-     printf("data 1 = %d , data2 = %d \n",data1, data2);
-
-     int result1 = data1 +data2;
-     int result2 = data1*data2;
-     int result3 = data1/data2;
-     int result4 = data1%data2;
-     printf("result : %d %d %d %d \n",result1,result2,result3,result4);
+    int sum =0, num;
+    for(num=1; num <=5;num++){
+        printf("num(%d) + sum(%d) = ",num,sum);
+        sum +=num;
+        printf("%d\n",sum);
+    }
+    printf("Result : num = %d sum =%d \n",num,sum);
  }

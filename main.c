e59aa3d75e897702f3cdea63334c94699d18f26c
Koduhwan <rhenghks5167@naver.com>
2023-08-26 17:23:32 +0900

    day_3

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
- void main() {
+ int main() {
 
-    int sum =0, num;
-    for(; ;){
-        printf("num(%d) + sum(%d) = ",num,sum);
-        sum +=num;
+    int sum =0, num= 1;
+    while(num<=5){
+        printf("num(%d) + sum (%d) = ",num,sum);
+        sum = sum + num;
         printf("%d\n",sum);
-        num++
-        if(num > 5)break;
+        num++;
     }
+
     printf("Result : num = %d sum =%d \n",num,sum);
+    return 0;
  }

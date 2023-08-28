35c27b9ce7fb5d1f36e6e5815a08f3014eb98133
Koduhwan <rhenghks5167@naver.com>
2023-08-28 19:40:44 +0900

    2의 보수와 부호가 없는 2진수 계산

--- main.c
+++ main.c
@@ -1,31 +1,19 @@
 #include <stdio.h>
 
- unsigned char GetBit(unsigned char dest_data, unsigned char bit_num){
-    unsigned char bit_state = 0;
-    //1바이트 변수라 0~7 자리까지 비트 이동 가능
-    if(bit_num < 8){
-        bit_state = dest_data & (0x01 << bit_num);
-        bit_state = bit_state >> bit_num;
-    }
-    return bit_state;
-}
-
-
  void main() {
 
-    unsigned char lamp_state = 0x75;    // 16진수 75 는 2진수로 0111 0101
-    unsigned  char bit_state;
-    int i;
 
-    printf("%X -> ", lamp_state);    //변경 전 값을 16진법으로 출력
-    // 8개의 비트 값을 모두 출력하기 위해 8번 반복함.
+    unsigned char A = 0x1A;
+    char B = 0x1A;
 
-    for(i=0;i<8;i++){
-        bit_state = GetBit(lamp_state,7 - i);
-        // 비트 값을 출력
-        printf("%d", bit_state);
-    }
-     printf("\n");
+\
+    A = A|0xFF;         // unsigned 형이라 그대로 계산
+    B = B|0xFF;         // signed라 부호가 있어 2의 보수로 전환 후 계산
+    printf("%d %d",A,B);
+
+
+
+
 
 }
 

cad89a5127591c32d86a0bc3add78ba89f5a500a
Koduhwan <rhenghks5167@naver.com>
2023-08-28 20:07:26 +0900

    day-4

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+ int result;
  int Sum(int data1, int data2){
     int result = data1 + data2;
     return result;
@@ -6,14 +7,14 @@
 
 }
  int main() {
-    int result;
-    result = Sum(5,3);
 
+
+     Sum(5,3);
     printf("5 + 3 = %d\n",result);
 
 
 
-
+    return 0 ;
 
 }
 

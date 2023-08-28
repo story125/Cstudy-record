cd8bb61f05bf9ddfe995be5faa1c98812c397c72
Koduhwan <rhenghks5167@naver.com>
2023-08-28 09:50:04 +0900

    day-4

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-
-
  void main() {
 
-    int m;
-    for(m=5; m<8;m++){
-        if(m==6)continue;
-        printf("%m(%d)\n",m);
+    int m, n;
+    for(m=5; m<7;m++){
+        for(n=7;n<3;n++) {
+            if (m == 5 && n == 1)goto exit_pos;
+            printf("m(%d)-n(%d)\n", m, n);
+        }
     }
-
+    exit_pos:
+     printf("< end > \n");
+    // 교재에는 printf(" < end > \n",m,n); 표기 되어있는데 오타??
 
 }

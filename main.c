9b942f0d615ce6bf85a2c810f03054a63313da83
Koduhwan <rhenghks5167@naver.com>
2023-08-30 10:18:08 +0900

    day_6

--- main.c
+++ main.c
@@ -3,12 +3,14 @@
 
  int main() {
 
-    int data_length;        // 문자열 길이를 저장할 변수
-    char data[]= "happy!";  //  happy 문자열 저장
-    //data 변수에 들어 있는 문자열 길이를 구함
-    data_length = strlen(data);
-    //문자열 길이 출력
-     printf("data length = %d\n", data_length);
+
+    char data[] = "abc";    //"abc" 문자열을 저장
+    char result[16];        // 새로운 문자열을 저장할 변수
+
+    strcpy(result, data);
+    strcat(result,"def");
+     printf("%s + \"def\" = %s \n",data, result);
+
      return 0 ;
 }
 

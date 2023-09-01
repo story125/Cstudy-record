85f3286500a202e439c48f478bfbce4ce847f77f
Koduhwan <rhenghks5167@naver.com>
2023-09-01 18:52:54 +0900

    gets 함수로 문자열을 입력 받을 때 입력 취소까지 처리 **(clion에서는 입력 중 최소가 안됨)

--- main.c
+++ main.c
@@ -4,11 +4,15 @@
 
  int main() {
 
-   //문자열은 마지막에 NULL 문자 0을 포함해야 하므로 최대 9개의 문자까지 저장가능함
-   char input_string[10];
-     gets(input_string); // 문자열을 입력 받아 input_string에 저장함
-     printf("input : %s\n",input_string);
 
+    char input_string[10];
+    // 반환값이 NULL이 아니라면 문자열을 입력 받아서 input_string에 저장함
+    if(NULL != gets(input_string)){
+        printf("input :%s\n",input_string);
+    }
+    else{
+        printf("input -> Cancled\n");
+    }
      return 0 ;
 }
 

c93efaacc5d7218be440cae81c4f8e52427703fc
Koduhwan <rhenghks5167@naver.com>
2023-09-01 18:19:11 +0900

    day-8

--- main.c
+++ main.c
@@ -1,24 +1,15 @@
 #include <stdio.h>
 
- int GetData(void *p_data, char type){
-
-    int result =0;
-    // type 변수에 저장된 값을 기준 으로 형 변환을 다르게 함.
-    if(type == 1)result = *(char *)p_data;
-    else if(type ==2) result = *(short *)p_data;
-    else if(type ==4) result = *(int *)p_data;
-     return result;
-
-}
 
 
  int main() {
 
-    int data = 0x12345678;
-    // data변수에서 2바이트 크기만 출력
-     printf("%X \n", GetData(&data,2));
-
-
+    int input_data;
+    // 표준 입력 함수를 사용하여 입력값을 하나만 받음
+    input_data = getchar();
+     printf("input : %c\n",input_data);
+     input_data = getchar();
+     printf("Second input : %c\n",input_data);
 
      return 0 ;
 }

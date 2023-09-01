63b53bfd8d920f2e1cc6b93f7d5f4f9dd55c4d7b
Koduhwan <rhenghks5167@naver.com>
2023-09-01 10:13:17 +0900

    void *를 사용하여 대상 메모리의 크기 조절하기

--- main.c
+++ main.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 
- void swap(int *pa , int *pb){
-    int temp =*pa;
-    *pa = *pb;
-    *pb= temp;
+ int GetData(void *p_data, char type){
+
+    int result =0;
+    // type 변수에 저장된 값을 기준 으로 형 변환을 다르게 함.
+    if(type == 1)result = *(char *)p_data;
+    else if(type ==2) result = *(short *)p_data;
+    else if(type ==4) result = *(int *)p_data;
+     return result;
 
 }
+
+
  int main() {
-    int start = 96, end =5;
-     printf("before : start = %d, end = %d\n",start,end);
-     if(start>end){
-         swap(&start,&end);
-     }
-     printf("after : start = %d, end = %d",start,end);
+
+    int data = 0x12345678;
+    // data변수에서 2바이트 크기만 출력
+     printf("%X \n", GetData(&data,2));
+
+
 
      return 0 ;
 }

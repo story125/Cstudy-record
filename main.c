0abbac7ee930586b3827af957e4bf386a94d7d65
Koduhwan <rhenghks5167@naver.com>
2023-09-01 18:22:23 +0900

    day-8

--- main.c
+++ main.c
@@ -7,8 +7,10 @@
     int input_data;
     // 표준 입력 함수를 사용하여 입력값을 하나만 받음
     input_data = getchar();
+    rewind(stdin);      // 표준 입력 버퍼에 있는 모든 값을 제거함
      printf("input : %c\n",input_data);
      input_data = getchar();
+     rewind(stdin);     // 똑같이 버퍼에 있는 모든 값을 제거함
      printf("Second input : %c\n",input_data);
 
      return 0 ;

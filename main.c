93dfea6f7de86ac74d64b7ea8bc20f27b9a9d6a2
Koduhwan <rhenghks5167@naver.com>
2023-09-04 09:55:03 +0900

    day-9

--- main.c
+++ main.c
@@ -1,38 +1,24 @@
 #include <stdio.h>
-
-
- int ArrayToInteger(char string[]){
-
-    int count = 0, num =0;
-    //문자열이 끝날 때 까지 반복
-    while(string[count] != 0){
-        // 반복할 때마다 이전 값에 10을 곱해서 자릿수를 증가시킴
-        num = num * 10 + string[count] - '0' ;
-        count++;
-    }
-    return num;
-
-}
-
-
-
  int main() {
 
-    int first_num, second_num;
-    char first_string[16], second_string[16];
+    int num = 0;
+    // 정상적인 나이를 입력할 떄까지 반복하기 위해 무한 반복을 사용함
 
-     printf("input first number : ");
-     gets(first_string);    // 첫 번쨰 문자열 입력
-     printf("input second number : ");
-     gets(second_string);    // 두 번째 문자열 입력
+    while(1){
 
-     first_num = ArrayToInteger(first_string);  // 문자열 -> 정수로 변환
-     second_num = ArrayToInteger(second_string);
+        printf("input age: ");
+        scanf("%d", &num);
 
-     printf("%d + %d = %d\n",first_num, second_num, first_num+ second_num);qq
+        if(num > 0 && num <=130){
+            break;
+        }
+        else{
+            printf("Incorrect Age \n");
 
+        }
+    }
 
-
+     printf( " your age : %d \n",num);
 
     return 0;
 }

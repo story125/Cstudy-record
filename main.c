d43844545ba0b61492cabbe963531941672797c5
Koduhwan <rhenghks5167@naver.com>
2023-09-09 18:50:38 +0900

    이중 포인터와 단일 포인터간의 call by value 와 call by address의 차이

--- main.c
+++ main.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 
- int main() {
+int global_val = 30;
 
-    short data =3;
-    short *p = &data;
-    short  **pp = &p;
-
-     printf("[Before  data : %d\n",data);
-     *p =4;
-     printf("[Use *p] data : %d\n",data);
-     **pp=5;
-     printf( "%d \n",data);
-
+void call_by_value(int *val)
+{
+    val = &global_val;
 }
 
+void call_by_refer(int **ref)       // 내가 담고자 하는 포인터 변수 즉 주소값을 저장하기 때문에 30으로 초기화 된다.
+{
+    *ref = &global_val;
+}
 
+int main()
+{
+    int local_val = 10;
+    int *value = &local_val;
+    int *refer = &local_val;
 
-
-
-
-
+    printf("before : *value=%d, *refer=%d\n", *value, *refer);
+    call_by_value(value);
+    call_by_refer(&refer);
+    printf("after  : *value=%d, *refer=%d\n", *value, *refer);
+}

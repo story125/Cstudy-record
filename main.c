3365e85fe71ecf420f0dd7e012b6461c567146cc
Koduhwan <rhenghks5167@naver.com>
2023-09-10 15:57:55 +0900

    day-12

--- main.c
+++ main.c
@@ -1,25 +1,17 @@
 #include <stdio.h>
+#include <malloc.h>
 
-int global_val = 30;
-
-void call_by_value(int *val)
-{
-    val = &global_val;
-}
-
-void call_by_refer(int **ref)       // 내가 담고자 하는 포인터 변수 즉 주소값을 저장하기 때문에 30으로 초기화 된다.
-{
-    *ref = &global_val;
+void GetMyData(int **q){
+    *q = (int *) malloc(8);
 }
 
 int main()
 {
-    int local_val = 10;
-    int *value = &local_val;
-    int *refer = &local_val;
+    int *p;
+    GetMyData(&p);
+    *p = 5;
+    free(p);
 
-    printf("before : *value=%d, *refer=%d\n", *value, *refer);
-    call_by_value(value);
-    call_by_refer(&refer);
-    printf("after  : *value=%d, *refer=%d\n", *value, *refer);
-}
+
+    return 0;
+  }

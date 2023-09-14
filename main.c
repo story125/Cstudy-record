2b0d6a97e265725ae972ceb33b3938bbe3e78a28
Koduhwan <rhenghks5167@naver.com>
2023-09-14 17:26:51 +0900

    day-14

--- main.c
+++ main.c
@@ -1,74 +1,51 @@
 #include <stdio.h>
-#define MAX_COUNT 5
-
-    typedef struct People
+#include <malloc.h>
+    typedef struct node
     {
-        char name[14];
-        unsigned short int age;
-        float height;
-        float weight;
-    } Person;
+        int number;
+        struct node *p_next;
+    } NODE;
 
-int AddFriend(Person *p_friend, int count){
-    if(count < MAX_COUNT){
-        p_friend = p_friend + count;
-        printf("\n 새로운 친구 정보를 입력하세요\n");
-        printf("1. 이름 : ");
-        scanf("%s ",p_friend ->name);
-        printf("2. 나이 : ");
-        scanf("%hu ",p_friend ->age);
-        printf("3. 키 : ");
-        scanf("%f ",p_friend ->height);
-        printf("4. 몸무게 : ");
-        scanf("%f ",p_friend ->weight);
-        printf("입력을 완료했습니다\n");
-        return 1;
+void AddNumber(NODE **pp_head, NODE **pp_tail, int data){
+    if(*pp_head != NULL){
+
+        (*pp_tail)->p_next =(NODE *) malloc(sizeof (NODE));
+        *pp_tail - (*pp_tail)->p_next;
+    }else{
+        *pp_head = (NODE *) malloc(sizeof (NODE));
+        *pp_tail = (*pp_tail)->p_next;
     }
-    else {
-        printf("최대 인원을 초과하여 입력할 수 없읍니다.\n");
-    }
-    return 0;
-
-}
-
-void ShowFriendList(Person *p_friend , int count){
-    int i;
-    if(count >0){
-        printf("\n등록된 친구목록\n");
-        printf("============================\n");
-        for(i=0;i<count;i++){
-            printf("%-14s, %3d, %6.2f\n",p_friend->name,p_friend->age,
-                   p_friend->height, p_friend->weight);
-            p_friend++;
-        }
-        printf("============================\n");
-    }else printf("\n 등록된 친구가 없습니다\n");
+    (*pp_tail)->number = data;
+    (*pp_tail)->p_next = NULL;
 }
 
 int main(){
-    Person friends[MAX_COUNT];
-    int count = 0, num;
-    while(1){
 
-        printf("        [메뉴]        \n");
-        printf("============================\n");
-        printf("1. 친구추가         \n");
-        printf("2. 친구 목록 보기         \n");
-        printf("3. 종료         \n");
-        printf("============================\n");
-        printf("번호 선택 : ");
-        scanf("%d",&num);
-        if(num == 1){
-            if(1 == AddFriend(friends, count))count++;
-            else if(num == 2){
-                ShowFriendList(friends,count);
-            } else if(num == 3){
-                break;
-            }
-            else printf("1~3번호만 입력할 수 있습니다\n]n");
-        }
+    NODE *p_head = NULL, *p_tail = NULL , *p;
+    int sum= 0, temp;
+
+    while(1){
+        printf("숫자를 입력하세요 (9999를 누르면 종료) : ");
+        scanf("%d",&temp);
+        if(temp == 9999)break;
+        AddNumber(&p_head,&p_tail,temp);
     }
 
+    p = p_head;
+    while (p != NULL){
+        if(p!=p_head)printf(" + ");
+        printf("  %d  ",p->number);
+        sum = sum + p->number;
+        p = p->p_next;
+    }
+    printf(" = %d\n",sum);
+
+    while (p_head != NULL){
+        p = p_head;
+        p_head = p_head ->p_next;
+        free(p);
+    }
+    p_tail = p_head;
 
 
     return 1;

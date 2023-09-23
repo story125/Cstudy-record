46f6807d78ae0a7994ca30c21e23b9e8230e63ab
Koduhwan <rhenghks5167@naver.com>
2023-09-23 20:00:16 +0900

    day-15

--- main.c
+++ main.c
@@ -1,52 +1,14 @@
 #include <stdio.h>
-#include <malloc.h>
-    typedef struct node
-    {
-        int number;
-        struct node *p_next;
-    } NODE;
 
-void AddNumber(NODE **pp_head, NODE **pp_tail, int data){
-    if(*pp_head != NULL){
+int main() {
+    int num1;
+    FILE *p_file = fopen("C:\\Users\\rheng\\CLionProjects\\tipsoft.txt", "rt");
+    if (NULL != p_file) {
+        fscanf(p_file, "%d", &num1);
+        printf( "%d\n", num1);
 
-        (*pp_tail)->p_next =(NODE *) malloc(sizeof (NODE));
-        *pp_tail - (*pp_tail)->p_next;
-    }else{
-        *pp_head = (NODE *) malloc(sizeof (NODE));
-        *pp_tail = (*pp_tail)->p_next;
+        fclose(p_file);
     }
-    (*pp_tail)->number = data;
-    (*pp_tail)->p_next = NULL;
+    return 0;
 }
 
-int main(){
-
-    NODE *p_head = NULL, *p_tail = NULL , *p;
-    int sum= 0, temp;
-
-    while(1){
-        printf("숫자를 입력하세요 (9999를 누르면 종료) : ");
-        scanf("%d",&temp);
-        if(temp == 9999)break;
-        AddNumber(&p_head,&p_tail,temp);
-    }
-
-    p = p_head;
-    while (p != NULL){
-        if(p!=p_head)printf(" + ");
-        printf("  %d  ",p->number);
-        sum = sum + p->number;
-        p = p->p_next;
-    }
-    printf(" = %d\n",sum);
-
-    while (p_head != NULL){
-        p = p_head;
-        p_head = p_head ->p_next;
-        free(p);
-    }
-    p_tail = p_head;
-
-
-    return 1;
-}
